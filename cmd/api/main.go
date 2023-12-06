8a7684d53430be0027c2b855b985054bf14baa2f
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-12-06 15:20:07 -0600

    Update main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,22 +1,43 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 )
 
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
 func main() {
+	var cfg config
+
+	flag.IntVar()
+
 	fmt.Println("hello")
-	http.HandleFunc("/v1/healthcheck", healthcheck) //api path? and function to execute when getting incoming requests?
-	err := http.ListenAndServe(":4000", nil)        // port and nil - which means it defaults to the go default servemux - problematic because it is not secure
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", healthcheck) //api endpoint and function to execute when getting incoming requests?
+	err := http.ListenAndServe(":4000", mux)       //using a defined serve mux like this prevents someone else from redefining the global variable used if done as nil, thus making it more secure
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
+// This is the healthcheck endpoint
 func healthcheck(w http.ResponseWriter, r *http.Request) {
-	env := "dev"
+	if r.Method != http.MethodGet {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	// env := "dev"
+
 	fmt.Fprintln(w, "status: available")
 	fmt.Fprintf(w, "environment: %s\n", env)
-	fmt.Fprintf(w, "version: %s\n", "1.0.0")
+	fmt.Fprintf(w, "version: %s\n", version)
 }
