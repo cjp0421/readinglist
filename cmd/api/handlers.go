047198298b5c6f8652864871677349d7df258059
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-12-07 09:49:24 -0600

    updated handlers.go

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -13,5 +13,5 @@ func (app *application) healthcheck (w http.ResponseWriter, r *http.Request){
 
 	fmt.Fprintln(w, "status: available")
 	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", app.config.version)
+	fmt.Fprintf(w, "version: %s\n", version)
 }
