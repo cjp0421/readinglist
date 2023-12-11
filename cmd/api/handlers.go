87d4f060f8c8ad47babfe1ea2b036af987945ed9
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-12-11 10:36:30 -0600

    continuing to work on building out endpoint handlers; fixed syntax errors

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
-//app method handling healthcheck endpoint
-func (app *application) healthcheck (w http.ResponseWriter, r *http.Request){
+// app method handling healthcheck endpoint
+func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 		return
@@ -17,37 +18,45 @@ func (app *application) healthcheck (w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "version: %s\n", version)
 }
 
-//This is a Handler - an app method handling getting and creating new books within the total list of books
-func (app *application) getCreateBooksHandler(w http.ResponseWriter, r http.Request){
-//the if statement validates that the request at this endpoint is only either GET or POST
+// This is a Handler - an app method handling getting and creating new books within the total list of books
+func (app *application) getCreateBooksHandler(w http.ResponseWriter, r *http.Request) {
+	//the if statement validates that the request at this endpoint is only either GET or POST
 
 	//if the endpoint /v1/books is used with get, it does the following
 	if r.Method == http.MethodGet {
 		fmt.Fprintln(w, "Display a list of books on the reading list")
 	}
-//if the endpoint /v1/books is used with post, it does the following
+	//if the endpoint /v1/books is used with post, it does the following
 	if r.Method == http.MethodPost {
 		fmt.Fprintln(w, "Added a new book to the reading list")
 	}
 }
 
-//This is another Handler (starts at 745) - an app method handling the get, update, deleting specific books
-//Below is a request multiplexer (aka a request router). It routes incoming requests to a handler using a set of rules
-func (app *application) getUpdateDeleteBooksHandler(w http.ResponseWriter, r http.Request){
+// This is another Handler (starts at 745) - an app method handling the get, update, deleting specific books
+// Below is a request multiplexer (aka a request router). It routes incoming requests to a handler using a set of rules
+func (app *application) getUpdateDeleteBooksHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
-	case http.MethodGet: 
-		app.getBook(w,r)	
+	case http.MethodGet:
+		app.getBook(w, r)
 
 	case http.MethodPut:
-		app.udpateBook(w,r)
-	
+		app.udpateBook(w, r)
+
 	case http.MethodDelete:
-		app.deleteBook(w,r)
+		app.deleteBook(w, r)
 
 	default:
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-		}
 	}
+}
 
 //Below is the definition of each specific case above
-func (app *application) getBook()
+
+// getting a specific book
+func (app *application) getBook(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("v1/books/"):]
+	idInt := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+	}
+}
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -2,19 +2,19 @@ package main
 
 import "net/http"
 
-//This instantiates all of the routes
-//this is a method tied to application (it takes in app, defined in main.go as an instance of the struct type application) that returns a new ServeMux
+// This instantiates all of the routes
+// this is a method tied to application (it takes in app, defined in main.go as an instance of the struct type application) that returns a new ServeMux
 func (app *application) route() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/v1/healthcheck", app.healthcheck) // this is an route
 	// Endpoints are functions available through the API
 	// A route is the name you use to access endpoints, used in the URL
-	
+
 	mux.HandleFunc("/v1/books", app.getCreateBooksHandler) // Gets all books with the GET method, Creates new book with the POST method
 	//1st arg is the route; 2nd arg is the handler function (endpoint)
-	
+
 	mux.HandleFunc("/v1/books/", app.getUpdateDeleteBooksHandler) // Handles queries related to individual books
-	
+
 	//This returns the mux and all the handlers associated with it
 	return mux
 }
