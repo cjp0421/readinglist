a91685cacc4bf3a08ee9286da62fca13384d3146
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-12-28 11:50:14 -0600

    updated helper to use headers

--- cmd/api/helper.go
+++ cmd/api/helper.go
@@ -13,13 +13,17 @@ type envelope map[string]any
 
 // Credit: Alex Edwards, Let's Go Further
 // This was added to replace duplicated code in the handlers so as to observed the DRY principle
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any) error {
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return http.ErrAbortHandler
 	}
 
 	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
