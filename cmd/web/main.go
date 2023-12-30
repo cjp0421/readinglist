e193987acb6a186fed6037963c034eaa48da4d61
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-12-30 15:01:09 -0600

    finished refactoring to use the readinglist model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,15 +4,21 @@ import (
 	"flag"
 	"log"
 	"net/http"
+
+	"readinglist/internal/models"
 )
 
 type application struct {
+	readinglist *models.ReadinglistModel
 }
 
 func main() {
 	addr := flag.String("addr", ":80", "HTTP network address") //using a flag means we can change it with command line arguments
+	endpoint := flag.String("endpoint", "http://localhost:4000/v1/books", "Endpoint for the readinglist web service")
 
-	app := &application{}
+	app := &application{
+		readinglist: &models.ReadinglistModel{Endpoint: *endpoint},
+	}
 
 	srv := &http.Server{
 		Addr:    *addr,
--- internal/models/readinglist.go
+++ internal/models/readinglist.go
@@ -53,3 +53,31 @@ func (m *ReadinglistModel) GetAll() (*[]Book, error) {
 
 	return booksResp.Books, nil
 }
+
+func (m *ReadinglistModel) Get(id int64) (*Book, error) {
+	url := fmt.Sprintf("%s/%d", m.Endpoint, id)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status: %s", resp.Status)
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var bookResp BookResponse
+
+	err = json.Unmarshal(data, &bookResp)
+	if err != nil {
+		return nil, err
+	}
+
+	return bookResp.Book, nil
+}
