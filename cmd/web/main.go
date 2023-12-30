c057751926a72ee9000716f8356aee495dd9365b
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-12-30 15:52:01 -0600

    added notes as comments explaining parts of the code

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,11 +13,11 @@ type application struct {
 }
 
 func main() {
-	addr := flag.String("addr", ":80", "HTTP network address") //using a flag means we can change it with command line arguments
-	endpoint := flag.String("endpoint", "http://localhost:4000/v1/books", "Endpoint for the readinglist web service")
+	addr := flag.String("addr", ":80", "HTTP network address")                                                        //using a flag means we can change it with command line arguments
+	endpoint := flag.String("endpoint", "http://localhost:4000/v1/books", "Endpoint for the readinglist web service") //this defines the endpoint for the readinglist
 
 	app := &application{
-		readinglist: &models.ReadinglistModel{Endpoint: *endpoint},
+		readinglist: &models.ReadinglistModel{Endpoint: *endpoint}, //this sets the endpoint as a pointer to the endpoint flag that was passed in
 	}
 
 	srv := &http.Server{
--- internal/models/readinglist.go
+++ internal/models/readinglist.go
@@ -7,7 +7,8 @@ import (
 	"net/http"
 )
 
-type Book struct {
+// the 3 types below allow us to unmarshall json
+type Book struct { //type for each book in the envelopes
 	ID        int64    `json:"id"`
 	Title     string   `json:"title"`
 	Published int      `json:"published"`
@@ -16,20 +17,22 @@ type Book struct {
 	Rating    float32  `json:"rating"`
 }
 
-type BookResponse struct {
-	Book *Book `json:"book"`
+type BookResponse struct { //type for enveloped single-book json responses
+	Book *Book `json:"book"` //pointer to the book
 }
 
-type BooksResponse struct {
-	Books *[]Book `json:"books"`
+type BooksResponse struct { //type for enveloped multi-book json responses
+	Books *[]Book `json:"books"` //pointer to a slice of books
 }
 
-type ReadinglistModel struct {
-	Endpoint string
+type ReadinglistModel struct { //this type is what all of the methods "hang on to"
+	Endpoint string //this is the url to the web service
 }
 
-func (m *ReadinglistModel) GetAll() (*[]Book, error) {
-	resp, err := http.Get(m.Endpoint)
+// the method below returns all of the book records in the database for the homepage
+// it doesn't take any values but it returns a slice of books and an error
+func (m *ReadinglistModel) GetAll() (*[]Book, error) { //it is a method that hangs off of the dereferenced pointer to ReadinglistModel
+	resp, err := http.Get(m.Endpoint) //this is what passes the url into the web service
 	if err != nil {
 		return nil, err
 	}
@@ -39,23 +42,24 @@ func (m *ReadinglistModel) GetAll() (*[]Book, error) {
 		return nil, fmt.Errorf("unexpected status: %s", resp.Status)
 	}
 
-	data, err := io.ReadAll(resp.Body)
+	data, err := io.ReadAll(resp.Body) //this reads the response body and puts it into a variable called data
 	if err != nil {
 		return nil, err
 	}
 
-	var booksResp BooksResponse
+	var booksResp BooksResponse //this handles the envelope
 
-	err = json.Unmarshal(data, &booksResp)
+	err = json.Unmarshal(data, &booksResp) //this unmarshalls the response in the data variable and puts it into the booksResp variable
 	if err != nil {
 		return nil, err
 	}
 
-	return booksResp.Books, nil
+	return booksResp.Books, nil //this returns the specific books data inside the envelope (does not return the envelope)
 }
 
+// this method takes in an id and returns a pointer to a book and an error - it returns a specific book by id
 func (m *ReadinglistModel) Get(id int64) (*Book, error) {
-	url := fmt.Sprintf("%s/%d", m.Endpoint, id)
+	url := fmt.Sprintf("%s/%d", m.Endpoint, id) //this makes the url variable contain a string with the endpoint and the id; it formats it fit the url style
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -72,12 +76,12 @@ func (m *ReadinglistModel) Get(id int64) (*Book, error) {
 		return nil, err
 	}
 
-	var bookResp BookResponse
+	var bookResp BookResponse //this creates a variable of type BookResponse (singular) to deal with then envelope
 
 	err = json.Unmarshal(data, &bookResp)
 	if err != nil {
 		return nil, err
 	}
 
-	return bookResp.Book, nil
+	return bookResp.Book, nil //this returns the singular book without the envelope
 }
